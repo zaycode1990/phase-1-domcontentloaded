dbcd7b931acb9d0daf1c4b1012685690c23972a1
Isaiah <ijandicos90@gmail.com>
2022-04-25 21:51:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=> {
+   const newText = document.getElementById("text")
+    return newText.textContent = "This is really cool!"
+})
